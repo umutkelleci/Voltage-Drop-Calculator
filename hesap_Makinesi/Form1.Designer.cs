9b07500305cdcbc4b77c05fc91ca1e6b8fd33104
umutkelleci <97851188+umutkelleci@users.noreply.github.com>
2022-01-26 00:40:27 +0300

    Add files via upload

---
+++ hesap_Makinesi/Form1.Designer.cs
@@ -0,0 +1,327 @@
+﻿
+namespace hesap_Makinesi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbvoltage = new System.Windows.Forms.ComboBox();
+            this.cmbfase = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtL = new System.Windows.Forms.TextBox();
+            this.txtN = new System.Windows.Forms.TextBox();
+            this.txtS = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cmbSystemVarient = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lblresult = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(269, 137);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SİSTEM VOLTAJI:";
+            // 
+            // cmbvoltage
+            // 
+            this.cmbvoltage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cmbvoltage.FormattingEnabled = true;
+            this.cmbvoltage.Items.AddRange(new object[] {
+            "24",
+            "42",
+            "220",
+            "380"});
+            this.cmbvoltage.Location = new System.Drawing.Point(417, 136);
+            this.cmbvoltage.Name = "cmbvoltage";
+            this.cmbvoltage.Size = new System.Drawing.Size(174, 28);
+            this.cmbvoltage.TabIndex = 1;
+            // 
+            // cmbfase
+            // 
+            this.cmbfase.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cmbfase.FormattingEnabled = true;
+            this.cmbfase.Items.AddRange(new object[] {
+            "1",
+            "3"});
+            this.cmbfase.Location = new System.Drawing.Point(417, 195);
+            this.cmbfase.Name = "cmbfase";
+            this.cmbfase.Size = new System.Drawing.Size(174, 28);
+            this.cmbfase.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(367, 196);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "FAZ:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(272, 258);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(139, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "KABLO BOYU(m):";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(329, 315);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "GÜÇ(kW):";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(245, 374);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(166, 20);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "KABLO KESİTİ(mm2):";
+            // 
+            // txtL
+            // 
+            this.txtL.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtL.Location = new System.Drawing.Point(417, 252);
+            this.txtL.Name = "txtL";
+            this.txtL.Size = new System.Drawing.Size(174, 26);
+            this.txtL.TabIndex = 7;
+            // 
+            // txtN
+            // 
+            this.txtN.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtN.Location = new System.Drawing.Point(417, 309);
+            this.txtN.Name = "txtN";
+            this.txtN.Size = new System.Drawing.Size(174, 26);
+            this.txtN.TabIndex = 8;
+            // 
+            // txtS
+            // 
+            this.txtS.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtS.Location = new System.Drawing.Point(417, 368);
+            this.txtS.Name = "txtS";
+            this.txtS.Size = new System.Drawing.Size(174, 26);
+            this.txtS.TabIndex = 9;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnCalculate.Location = new System.Drawing.Point(348, 423);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(120, 49);
+            this.btnCalculate.TabIndex = 10;
+            this.btnCalculate.Text = "HESAPLA";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnClear.Location = new System.Drawing.Point(507, 423);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(120, 49);
+            this.btnClear.TabIndex = 11;
+            this.btnClear.Text = "TEMİZLE";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Enabled = false;
+            this.txtResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtResult.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.txtResult.Location = new System.Drawing.Point(664, 164);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(238, 115);
+            this.txtResult.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(777, 502);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(125, 20);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "UMUT KELLECİ";
+            // 
+            // cmbSystemVarient
+            // 
+            this.cmbSystemVarient.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cmbSystemVarient.FormattingEnabled = true;
+            this.cmbSystemVarient.Items.AddRange(new object[] {
+            "AYDINLATMA VE PRİZ",
+            "MOTOR"});
+            this.cmbSystemVarient.Location = new System.Drawing.Point(417, 89);
+            this.cmbSystemVarient.Name = "cmbSystemVarient";
+            this.cmbSystemVarient.Size = new System.Drawing.Size(174, 26);
+            this.cmbSystemVarient.TabIndex = 15;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(289, 90);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(122, 20);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "SİSTEM TÜRÜ:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(207, 82);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(12, 100);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(207, 37);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Web Adresi";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(660, 315);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(70, 20);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "SONUÇ:";
+            // 
+            // lblresult
+            // 
+            this.lblresult.AutoSize = true;
+            this.lblresult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblresult.Location = new System.Drawing.Point(739, 315);
+            this.lblresult.Name = "lblresult";
+            this.lblresult.Size = new System.Drawing.Size(0, 20);
+            this.lblresult.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(939, 544);
+            this.Controls.Add(this.lblresult);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.cmbSystemVarient);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtS);
+            this.Controls.Add(this.txtN);
+            this.Controls.Add(this.txtL);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cmbfase);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cmbvoltage);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GERİLİM DÜŞÜM HESABI";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbvoltage;
+        private System.Windows.Forms.ComboBox cmbfase;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtL;
+        private System.Windows.Forms.TextBox txtN;
+        private System.Windows.Forms.TextBox txtS;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cmbSystemVarient;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lblresult;
+    }
+}
+
---
+++ hesap_Makinesi/Form1.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hesap_Makinesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double result;
+        double constant = 0.0124;
+        double constant2 = 0.074;
+        double constant3 = 1.0;
+        double constant4 = 6.2;
+        double n;
+        double s;
+        double l;
+        private void Clear()
+        {
+            txtL.Clear();
+            txtN.Clear();
+            txtS.Clear();
+            cmbfase.Text = "";
+            cmbSystemVarient.Text = "";
+            cmbvoltage.Text = "";
+        }
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                int voltage = Convert.ToInt32(cmbvoltage.Text);
+                int fase = Convert.ToInt32(cmbfase.Text);
+   
+                if (txtN.Text.Contains("."))
+                {
+                    MessageBox.Show("LÜTFEN , KULLANINIZ");
+                    Clear();
+                   
+                }
+                else
+                {
+                     n = Convert.ToDouble(txtN.Text);
+                }
+
+                if (txtL.Text.Contains("."))
+                {
+                    MessageBox.Show("LÜTFEN , KULLANINIZ");
+                    Clear();
+
+                }
+                else
+                {
+                    l = Convert.ToDouble(txtL.Text);
+                }
+
+
+
+                if (txtS.Text.Contains("."))
+                {
+                    MessageBox.Show("LÜTFEN , KULLANINIZ");
+                    Clear();
+
+                }
+                else
+                {
+                    s = Convert.ToDouble(txtS.Text);
+                }
+
+
+
+                //380V ve trifaze olduğu zaman
+                if (voltage == 380 && fase == 3)
+                {
+
+                    result = (constant * l * n) / s;
+                }
+                if (cmbSystemVarient.Text == "AYDINLATMA VE PRİZ")
+                {
+                    if (result < 1.5)
+                    {
+                        txtResult.Text = "KABLO KESİTİ UYGUN";
+                        lblresult.Text = result.ToString();
+                    }
+                    else
+                    {
+                        txtResult.Text = "KABLO KESİTİNİ ARTTIRINIZ";
+                        lblresult.Text = result.ToString();
+                    }
+                }
+                if (cmbSystemVarient.Text == "MOTOR")
+                {
+                    if (result < 3.0)
+                    {
+                        txtResult.Text = "KABLO KESİTİ UYGUN";
+                        lblresult.Text = result.ToString();
+                    }
+                    else
+                    {
+                        txtResult.Text = "KABLO KESİTİNİ ARTTIRINIZ";
+                        lblresult.Text = result.ToString();
+                    }
+                }
+
+
+                //220V ve monofaze olduğu zaman
+
+                if (voltage == 220 && fase == 1)
+                {
+
+                    result = (constant2 * l * n) / s;
+                }
+
+                if (cmbSystemVarient.Text == "AYDINLATMA VE PRİZ")
+                {
+                    if (result < 1.5)
+                    {
+                        txtResult.Text = "KABLO KESİTİ UYGUN";
+                        lblresult.Text = result.ToString();
+                    }
+                    else
+                    {
+                        txtResult.Text = "KABLO KESİTİNİ ARTTIRINIZ";
+                        lblresult.Text = result.ToString();
+                    }
+                }
+                if (cmbSystemVarient.Text == "MOTOR")
+                {
+                    if (result < 3.0)
+                    {
+                        txtResult.Text = "KABLO KESİTİ UYGUN";
+                        lblresult.Text = result.ToString();
+                    }
+                    else
+                    {
+                        txtResult.Text = "KABLO KESİTİNİ ARTTIRINIZ";
+                        lblresult.Text = result.ToString();
+                    }
+                }
+
+
+                //42V ve trifaze olduğu zaman                
+
+                if (voltage == 42 && fase == 3)
+                {
+
+                    result = (constant3 * l * n) / s;
+                }
+
+                if (cmbSystemVarient.Text == "AYDINLATMA VE PRİZ")
+                {
+                    if (result < 1.5)
+                    {
+                        txtResult.Text = "KABLO KESİTİ UYGUN";
+                        lblresult.Text = result.ToString();
+                    }
+                    else
+                    {
+                        txtResult.Text = "KABLO KESİTİNİ ARTTIRINIZ";
+                        lblresult.Text = result.ToString();
+                    }
+                }
+                if (cmbSystemVarient.Text == "MOTOR")
+                {
+                    if (result < 3.0)
+                    {
+                        txtResult.Text = "KABLO KESİTİ UYGUN";
+                        lblresult.Text = result.ToString();
+                    }
+                    else
+                    {
+                        txtResult.Text = "KABLO KESİTİNİ ARTTIRINIZ";
+                        lblresult.Text = result.ToString();
+                    }
+                }
+
+                //24V ve monofaze olduğu zaman
+
+                if (voltage == 24 && fase == 1)
+                {
+
+                    result = (constant4 * l * n) / s;
+                }
+
+                if (cmbSystemVarient.Text == "AYDINLATMA VE PRİZ")
+                {
+                    if (result < 1.5)
+                    {
+                        txtResult.Text = "KABLO KESİTİ UYGUN";
+                        lblresult.Text = result.ToString();
+                    }
+                    else
+                    {
+                        txtResult.Text = "KABLO KESİTİNİ ARTTIRINIZ";
+                        lblresult.Text = result.ToString();
+                    }
+                }
+                if (cmbSystemVarient.Text == "MOTOR")
+                {
+                    if (result < 3.0)
+                    {
+                        txtResult.Text = "KABLO KESİTİ UYGUN";
+                        lblresult.Text = result.ToString();
+                    }
+                    else
+                    {
+                        txtResult.Text = "KABLO KESİTİNİ ARTTIRINIZ";
+                        lblresult.Text = result.ToString();
+                    }
+                }
+
+                //Hatalar
+                if (voltage == 380 && fase == 1)
+                {
+                    MessageBox.Show("YANLIŞ FAZ SEÇİMİ !", "HATA !");
+                    Clear();
+                }
+
+                if (voltage == 220 && fase == 3)
+                {
+                    MessageBox.Show("YANLIŞ FAZ SEÇİMİ !", "HATA !");
+                    Clear();
+                }
+
+                if (voltage == 42 && fase == 1)
+                {
+                    MessageBox.Show("YANLIŞ FAZ SEÇİMİ !", "HATA !");
+                    Clear();
+                }
+
+                if (voltage == 24 && fase == 3)
+                {
+                    MessageBox.Show("YANLIŞ FAZ SEÇİMİ !", "HATA !");
+                    Clear();
+                }
+
+            }
+
+            catch
+            {
+                MessageBox.Show("Lütfen değerleri eksiksiz giriniz!", "Hata!");
+                Clear();
+            }
+        }
+
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://noktamakina.com");
+            ////}
+
+            /*AYDINLATMA VE PRİZ
+    MOTOR*/
+        }
+    }
+}
